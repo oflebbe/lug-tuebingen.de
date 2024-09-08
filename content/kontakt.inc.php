3fa2697f96a3211b0e105d2d8ea8a9bee04e2c80
Christian Seiler <christian@iwakd.de>
2024-09-08 14:47:03 +0200

    Entfernen der alten Webseite

--- content/kontakt.inc.php
+++
@@ -1,6 +0,0 @@
-<h1>Kontakt</h1>
-
-Ansprechpartner bei der LUGT erreicht man unter <a
-href="mailto:lug-tuebingen-owner(at)listserv.uni-tuebingen.de">lug-tuebingen-owner&#64;listserv.uni-tuebingen.de</a>.<br /><br />
-Ansonsten sind die <a href="<?=$_SERVER['PHP_SELF']?>?view=treffen">regelmäßigen Treffen</a> eine
-gute Möglichkeit um Kontakte zu knüpfen.
