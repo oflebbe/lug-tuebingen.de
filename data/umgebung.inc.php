c3d7e201d7a44a3741740c84f07247e332aea49b
Christian Seiler <christian@iwakd.de>
2024-10-27 14:14:19 +0100

    Weitere Links zu Linux-nahen Dingen in Tübingen setzen

--- data/umgebung.inc.php
+++ data/umgebung.inc.php
@@ -7,8 +7,21 @@
 </ul>
 <p>Es gibt weiterhin auch noch ein <a href="https://www.linux-community.de/lug/">Verzeichnis aller Linux-Usergroups im deutschen Sprachraum</a> beim Linux Community Verlag.</p>
 <h3>Interessantes in Tübingen</h3>
+<p>Für Linux-Nutzer könnten möglicherweise auch folgende Ressourcen und Veranstaltungen in Tübingen interessant sein.</p>
+<h4>Vereine / Regelmäßige Treffen</h4>
 <ul>
   <li><a href="https://cttue.de/">Chaostreff Tübingen</a></li>
+  <li><a href="https://cryptoparty-tuebingen.de/">Cryptoparty Tübingen</a></li>
+</ul>
+<h4>Offene Werkstätten / Maker-Spaces in Tübingen</h4>
+<ul>
   <li><a href="https://www.fablab-neckar-alb.org/">FabLab Neckar-Alb e.V.</a></li>
+  <li><a href="https://ki-maker.space/">KI Maker Space Tübingen</a></li>
+  <li><a href="https://franzwerk-tuebingen.de/">FRANZ!werk Tübingen</a></li>
+  <li><a href="https://www.werkstadthaus.de/">Werkstadthaus Tübingen</a></li>
+</ul>
+<h4>Sonstiges:</h4>
+<ul>
+  <li><a href="https://www.tuebix.org/">tübix - Tübinger Linux-Tag</a> (1x pro Jahr)</li>
 </ul>
 <?php show_html_footer(); ?>
